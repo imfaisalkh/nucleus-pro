0963a00bb8c9bc02385c8e04b82816ef7ec646d4
Faisal Khurshid <imfaisalkh@gmail.com>
2021-02-22 16:07:05 +0500

    Updated to latest Elementor API + Fixed Meta Info Widget Issue.

--- plugin.php
+++ plugin.php
@@ -1,6 +1,8 @@
 <?php
 namespace Nucleus;
 
+use Nucleus\PageSettings\Page_Settings;
+
 // Define each widget class name here (EDIT BELOW # 1)
 use Nucleus\Widgets\Meta_info;
 use Nucleus\Widgets\Custom_Divider;
@@ -8,89 +10,168 @@ use Nucleus\Widgets\Price_Table;
 use Nucleus\Widgets\Portfolio_Module;
 use Nucleus\Widgets\Team_Module;
 use Nucleus\Widgets\Testimonial_Module;
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
 /**
- * Main Plugin Class
+ * Class Plugin
  *
- * Register new elementor widget.
- *
- * @since 1.0.0
+ * Main Plugin class
+ * @since 1.2.0
  */
 class Plugin {
 
 	/**
-	 * Constructor
+	 * Instance
 	 *
-	 * @since 1.0.0
+	 * @since 1.2.0
+	 * @access private
+	 * @static
 	 *
+	 * @var Plugin The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 *
+	 * @return Plugin An instance of the class.
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * widget_scripts
+	 *
+	 * Load required plugin core files.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function widget_scripts() {
+		wp_register_script( 'elementor-meta-info', plugins_url( '/assets/js/meta-info.js', __FILE__ ), [ 'jquery' ], false, true );
+	}
+
+	/**
+	 * Editor scripts
+	 *
+	 * Enqueue plugin javascripts integrations for Elementor editor.
+	 *
+	 * @since 1.2.1
+	 * @access public
+	 */
+	public function editor_scripts() {
+		add_filter( 'script_loader_tag', [ $this, 'editor_scripts_as_a_module' ], 10, 2 );
+
+		wp_enqueue_script(
+			'elementor-meta-info-editor',
+			plugins_url( '/assets/js/editor/editor.js', __FILE__ ),
+			[
+				'elementor-editor',
+			],
+			'1.2.1',
+			true
+		);
+	}
+
+	/**
+	 * Force load editor script as a module
+	 *
+	 * @since 1.2.1
+	 *
+	 * @param string $tag
+	 * @param string $handle
+	 *
+	 * @return string
+	 */
+	public function editor_scripts_as_a_module( $tag, $handle ) {
+		if ( 'elementor-meta-info-editor' === $handle ) {
+			$tag = str_replace( '<script', '<script type="module"', $tag );
+		}
+
+		return $tag;
+	}
+
+	/**
+	 * Include Widgets files
+	 *
+	 * Load widgets files
+	 *
+	 * @since 1.2.0
+	 * @access private
+	 */
+	private function include_widgets_files() {
+		// Register each widget here (EDIT BELOW # 2)
+		require_once( __DIR__ . '/widgets/meta-info.php' );
+		require_once( __DIR__ . '/widgets/custom-divider.php' );
+		require_once( __DIR__ . '/widgets/price-table.php' );
+		require_once( __DIR__ . '/widgets/portfolio-module.php' );
+		require_once( __DIR__ . '/widgets/team-module.php' );
+		require_once( __DIR__ . '/widgets/testimonials-module.php' );
+	}
+
+	/**
+	 * Register Widgets
+	 *
+	 * Register new Elementor widgets.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function register_widgets() {
+		// Its is now safe to include Widgets files
+		$this->include_widgets_files();
+
+		// Register Widgets (EDIT BELOW # 3)
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Meta_info() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Custom_Divider() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Price_Table() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Portfolio_Module() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Team_Module() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Testimonial_Module() );
+	}
+
+	/**
+	 * Add page settings controls
+	 *
+	 * Register new settings for a document page settings.
+	 *
+	 * @since 1.2.1
+	 * @access private
+	 */
+	private function add_page_settings_controls() {
+		require_once( __DIR__ . '/page-settings/manager.php' );
+		new Page_Settings();
+	}
+
+	/**
+	 *  Plugin class constructor
+	 *
+	 * Register plugin action hooks and filters
+	 *
+	 * @since 1.2.0
 	 * @access public
 	 */
 	public function __construct() {
-		$this->add_actions();
-	}
 
-	/**
-	 * Add Actions
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access private
-	 */
-	private function add_actions() {
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'on_widgets_registered' ] );
+		// Register widget scripts
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
 
-		add_action( 'elementor/frontend/after_register_scripts', function() {
-			wp_register_script( 'meta-info', plugins_url( '/assets/js/meta-info.js', ELEMENTOR_INIT__FILE__ ), [ 'jquery' ], false, true );
-		} );
-	}
+		// Register widgets
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
 
-	/**
-	 * On Widgets Registered
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access public
-	 */
-	public function on_widgets_registered() {
-		$this->includes();
-		$this->register_widget();
-	}
-
-	/**
-	 * Includes
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access private
-	 */
-	private function includes() {
-		// Register each widget here (EDIT BELOW # 2)
-		require __DIR__ . '/widgets/meta-info.php';
-		require __DIR__ . '/widgets/custom-divider.php';
-		require __DIR__ . '/widgets/price-table.php';
-		require __DIR__ . '/widgets/portfolio-module.php';
-		require __DIR__ . '/widgets/team-module.php';
-		require __DIR__ . '/widgets/testimonials-module.php';
-	}
-
-	/**
-	 * Register Widget
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access private
-	 */
-	private function register_widget() {
-		// Register each widget here (EDIT BELOW # 3)
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Meta_info() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Custom_Divider() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Price_Table() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Portfolio_Module() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Team_Module() );
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Testimonial_Module() );
+		// Register editor scripts
+		// add_action( 'elementor/editor/after_enqueue_scripts', [ $this, 'editor_scripts' ] );
+		
+		$this->add_page_settings_controls();
 	}
 }
 
-new Plugin();
+// Instantiate Plugin Class
+Plugin::instance();
--- widgets/meta-info.php
+++ widgets/meta-info.php
@@ -15,104 +15,144 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  */
 class Meta_info extends Widget_Base {
 
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
 	public function get_name() {
 		return 'meta-info';
 	}
 
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
 	public function get_title() {
-		return __( 'Meta Info', 'nucleus' );
+		return __( 'Meta Info', 'elementor-meta-info' );
 	}
 
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
 	public function get_icon() {
 		return 'eicon-alert';
 	}
 
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
 	public function get_categories() {
-		return [ 'general-elements' ];
+		return [ 'general' ];
 	}
 
 	/**
-	 * A list of scripts that the widgets is depended in
-	 * @since 1.3.0
-	 **/
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
 	public function get_script_depends() {
-		return [ 'meta-info' ];
+		return [ 'elementor-meta-info' ];
 	}
 
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function _register_controls() {
-		
-		// CONTENT - TAB
 		$this->start_controls_section(
 			'section_content',
 			[
-				'label' => __( 'Content', 'nucleus' ),
+				'label' => __( 'Content', 'elementor-meta-info' ),
+			]
+		);
+
+		$repeater = new \Elementor\Repeater();
+
+		$repeater->add_control(
+			'meta_label', [
+				'label' => __( 'Meta Label', 'nucleus-pro' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'label_block' => true,
+			]
+		);
+
+		$repeater->add_control(
+			'meta_value', [
+				'label' => __( 'Meta Value', 'nucleus-pro' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'label_block' => true,
 			]
 		);
 
 		$this->add_control(
 			'tabs',
 			[
-				'label' => __( 'Meta Fields', 'nucleus' ),
-				'type' => Controls_Manager::REPEATER,
+				'label' => __( 'Meta Fields', 'nucleus-pro' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => $repeater->get_controls(),
 				'default' => [
 					[
 						'meta_label' => __( 'Author', 'nucleus' ),
 						'meta_value' => __( 'Jhon Doe', 'nucleus' ),
 					],
 				],
-				'fields' => [
-					[
-						'name' => 'meta_label',
-						'label' => __( 'Lable', 'nucleus' ),
-						'type' => Controls_Manager::TEXT,
-						'label_block' => true,
-					],
-					[
-						'name' => 'meta_value',
-						'label' => __( 'Value', 'nucleus' ),
-						'type' => Controls_Manager::TEXT,
-						'label_block' => true,
-					],
-				],
-				'title_field' => '{{{ meta_label }}}', // what field value to show as repeater title
+				'title_field' => '{{{ meta_label }}}',
 			]
 		);
 
 		$this->end_controls_section();
 
-		// STYLE - TAB
-		$this->start_controls_section(
-			'section_style',
-			[
-				'label' => __( 'Style', 'nucleus' ),
-				'tab' => Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		$this->add_control(
-			'text_transform',
-			[
-				'label' => __( 'Text Transform', 'nucleus' ),
-				'type' => Controls_Manager::SELECT,
-				'default' => '',
-				'options' => [
-					'' => __( 'None', 'nucleus' ),
-					'uppercase' => __( 'UPPERCASE', 'nucleus' ),
-					'lowercase' => __( 'lowercase', 'nucleus' ),
-					'capitalize' => __( 'Capitalize', 'nucleus' ),
-				],
-				'selectors' => [
-					'{{WRAPPER}} .title' => 'text-transform: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->end_controls_section();
 	}
 
-	// RENDER ON FRONT-END ONLY
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function render() {
-		$settings = $this->get_settings();
+		$settings = $this->get_settings_for_display();
 		?>
 		<div class="meta-fields">
 			<?php
@@ -129,12 +169,20 @@ class Meta_info extends Widget_Base {
 		<?php
 	}
 
-	// RENDER WHILE EDITING ONLY
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function _content_template() {
 		?>
 		<div class="meta-fields" data-active-section="{{ editSettings.activeItemIndex ? editSettings.activeItemIndex : 0 }}">
 			<#
-			if ( settings.tabs ) {
+			if ( settings.tabs.length ) {
 				var counter = 1;
 				_.each( settings.tabs, function( item ) { #>
 					<div class="meta-field" data-count="{{ counter }}">
